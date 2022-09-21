1676e0b70fe6636d9d299b6767d8a8ad7ae86fda
MichalPokojny <michal.pokojny@hotmail.com>
2022-09-21 17:02:17 +0000

    Add function for adding list of rules when clicked

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,30 @@
+// changing color for word "Game"
+function changeColor(){
 let i = 0;
 let colors = ['black','blue', 'red', 'orange', 'pink'];
 // Triggers every two second
 function changeColor(){
     document.getElementById("game-heading").style.color = colors[i];
-    i++;
-    
+    i++; 
 }
 setInterval(changeColor, 2000);
+}
+changeColor();
+// Add rules text when clicked
+function rules (){
+    let rulesText = document.getElementById("rules-open");
+    rulesText.innerHTML = 
+   `<ul>
+    <li>Scissors cuts paper</li>
+    <li>Paper covers rock</li>
+    <li>Rock crushes lizard</li>
+    <li>Lizard poisons Spock</li>
+    <li>Spock smashes scissors</li>
+    <li>Scissors decapitates lizard</li>
+    <li>Lizard eats paper</li>
+    <li>Paper disproves Spock</li>
+    <li>Spock vaporizes rock, </li>
+    <li>Rock crushes scissors</li>
+    </ul>`   
+     
+} 
