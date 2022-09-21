a535813ff2b20dfc2854c573dad1e1af296bf009
MichalPokojny <michal.pokojny@hotmail.com>
2022-09-21 18:56:09 +0000

    Add event listeners for all buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,4 +31,29 @@ function rules (){
 let rulesButton = document.getElementById("rules-list")
 rulesButton.addEventListener("click", rules);
 
+//Declare variables for DOM elements
+ 
+let buttons = document.getElementsByClassName("btn");
+let playerScore = document.getElementById("player-score");
+let computerScore = document.getElementById("computer-score");
+let playerImage = document.getElementById("player-image");
+let computerImage = document.getElementById("computer-image");
+let message = document.getElementById("message");
+let choice = ["rock", "paper", "scissors", "lizard", "spock"]
+//Add event listeners to buttons
 
+for (let button of buttons) {
+    button.addEventListener("click", function() {
+        let playerChoice = this.getAttribute("data-choice");
+        playGame(playerChoice);
+    });
+}
+
+function playGame(playerChoice) {
+    playerImage.src = `assets/images/${choice[playerChoice]}.png`
+    playerImage.alt = choice[playerChoice];
+    
+    let computerChoice = Math.floor(Math.random() * 3);
+
+
+}
