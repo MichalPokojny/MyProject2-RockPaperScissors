ea8fc7bfeda38fff401a8d3bfb9bad4232dd82b4
MichalPokojny <michal.pokojny@hotmail.com>
2022-09-28 23:20:10 +0000

    format html,css,js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,6 @@
 // Declare variables for DOM elements
- 
-const choice = ["rock", "paper", "scissors", "lizard", "spock"];
-const colors = ['black','blue', 'red', 'orange', 'pink'];
+const CHOICE = ["rock", "paper", "scissors", "lizard", "spock"];
+const COLORS = ['black', 'blue', 'red', 'orange', 'pink'];
 let buttons = document.getElementsByClassName("btn");
 let playerScore = document.getElementById("player-score");
 let computerScore = document.getElementById("computer-score");
@@ -11,113 +10,91 @@ let message = document.getElementById("message");
 let rulesButton = document.getElementById("rules-list");
 
 
-
-// Add rules text when clicked
-
-rulesButton.addEventListener("click", rules);
-
-function rules (){
-    let rulesText = document.getElementById("rules-open");
-    if( rulesText.classList.contains("hide") ){
-    rulesText.classList.remove("hide");
-} else {
-    rulesText.classList.add("hide");
-}  
-}
-
-// Add event listeners to buttons
-
-for (let button of buttons) {
-    button.addEventListener("click", function() {
-        let playerChoice = parseInt(this.getAttribute("data-choice"));
-        playGame(playerChoice);
-    });
-}
-
 // Adding image to chosen gesture
 
 function playGame(playerChoice) {
-    playerImage.src = `assets/images/${choice[playerChoice]}.png`;
-    playerImage.alt = choice[playerChoice];
-    
+    playerImage.src = `assets/images/${CHOICE[playerChoice]}.png`;
+    playerImage.alt = CHOICE[playerChoice];
+
     let computerChoice = parseInt(Math.floor(Math.random() * 5));
-    computerImage.src = `assets/images/${choice[computerChoice]}.png`;
-    computerImage.alt = choice[computerChoice];
+    computerImage.src = `assets/images/${CHOICE[computerChoice]}.png`;
+    computerImage.alt = CHOICE[computerChoice];
     checkAnswer(playerChoice, computerChoice);
 }
 
 // Checks both answers and pritining result
 
-function checkAnswer (playerChoice, computerChoice) {
-    if(playerChoice == computerChoice) {
-        message.innerHTML= "Draw";
-    } else if (playerChoice === 0 && computerChoice === 1){
-        message.innerHTML= "Computer Wins!";
+function checkAnswer(playerChoice, computerChoice) {
+    if (playerChoice == computerChoice) {
+        message.innerHTML = "Draw";
+    } else if (playerChoice === 0 && computerChoice === 1) {
+        message.innerHTML = "Computer Wins!";
         ComputerWins();
-    } else if (playerChoice === 0 && computerChoice === 2){
-        message.innerHTML= "Player Wins!";
+    } else if (playerChoice === 0 && computerChoice === 2) {
+        message.innerHTML = "Player Wins!";
         playerWins();
-    } else if (playerChoice === 0 && computerChoice === 3){
-        message.innerHTML= "Player Wins!";
+    } else if (playerChoice === 0 && computerChoice === 3) {
+        message.innerHTML = "Player Wins!";
         playerWins();
-    } else if (playerChoice === 0 && computerChoice === 4){
-        message.innerHTML= "Computer Wins!";
+    } else if (playerChoice === 0 && computerChoice === 4) {
+        message.innerHTML = "Computer Wins!";
         ComputerWins();
-    } else if (playerChoice === 1 && computerChoice === 0){
-        message.innerHTML= "Player Wins!";
+    } else if (playerChoice === 1 && computerChoice === 0) {
+        message.innerHTML = "Player Wins!";
         playerWins();
-    } else if (playerChoice === 1 && computerChoice === 2){
-        message.innerHTML= "Computer Wins!";
+    } else if (playerChoice === 1 && computerChoice === 2) {
+        message.innerHTML = "Computer Wins!";
         ComputerWins();
-    } else if (playerChoice === 1 && computerChoice === 3){
-        message.innerHTML= "Computer Wins!";
+    } else if (playerChoice === 1 && computerChoice === 3) {
+        message.innerHTML = "Computer Wins!";
         ComputerWins();
-    } else if (playerChoice === 1 && computerChoice === 4){
-        message.innerHTML= "Player Wins!";
+    } else if (playerChoice === 1 && computerChoice === 4) {
+        message.innerHTML = "Player Wins!";
         playerWins();
-    } else if (playerChoice === 2 && computerChoice === 0){
-        message.innerHTML= "Computer Wins!";
+    } else if (playerChoice === 2 && computerChoice === 0) {
+        message.innerHTML = "Computer Wins!";
         ComputerWins();
-    } else if (playerChoice === 2 && computerChoice === 1){
-        message.innerHTML= "Player Wins!";
+    } else if (playerChoice === 2 && computerChoice === 1) {
+        message.innerHTML = "Player Wins!";
         playerWins();
-    } else if (playerChoice === 2 && computerChoice === 3){
-        message.innerHTML= "Player Wins!";
+    } else if (playerChoice === 2 && computerChoice === 3) {
+        message.innerHTML = "Player Wins!";
         playerWins();
-    } else if (playerChoice === 2 && computerChoice === 4){
-        message.innerHTML= "Computer Wins!";
+    } else if (playerChoice === 2 && computerChoice === 4) {
+        message.innerHTML = "Computer Wins!";
         ComputerWins();
-    } else if (playerChoice === 3 && computerChoice === 0){
-        message.innerHTML= "Computer Wins!";
+    } else if (playerChoice === 3 && computerChoice === 0) {
+        message.innerHTML = "Computer Wins!";
         ComputerWins();
-    } else if (playerChoice === 3 && computerChoice === 1){
-        message.innerHTML= "Player Wins!";
+    } else if (playerChoice === 3 && computerChoice === 1) {
+        message.innerHTML = "Player Wins!";
         playerWins();
-    } else if (playerChoice === 3 && computerChoice === 2){
-        message.innerHTML= "Computer Wins!";
+    } else if (playerChoice === 3 && computerChoice === 2) {
+        message.innerHTML = "Computer Wins!";
         ComputerWins();
-    } else if (playerChoice === 3 && computerChoice === 4){
-        message.innerHTML= "Player Wins!";
+    } else if (playerChoice === 3 && computerChoice === 4) {
+        message.innerHTML = "Player Wins!";
         playerWins();
-    } else if (playerChoice === 4 && computerChoice === 0){
-        message.innerHTML= "Player Wins!";
+    } else if (playerChoice === 4 && computerChoice === 0) {
+        message.innerHTML = "Player Wins!";
         playerWins();
-    } else if (playerChoice === 4 && computerChoice === 1){
-        message.innerHTML= "Computer Wins!";
+    } else if (playerChoice === 4 && computerChoice === 1) {
+        message.innerHTML = "Computer Wins!";
         ComputerWins();
-    } else if (playerChoice === 4 && computerChoice === 2){
-        message.innerHTML= "Player Wins!";
+    } else if (playerChoice === 4 && computerChoice === 2) {
+        message.innerHTML = "Player Wins!";
         playerWins();
-    } else if (playerChoice === 4 && computerChoice === 3){
-        message.innerHTML= "Computer Wins!";
+    } else if (playerChoice === 4 && computerChoice === 3) {
+        message.innerHTML = "Computer Wins!";
         ComputerWins();
-    }              
+    }
 }
 // Function for adding score for player or computer
-function playerWins(){
+function playerWins() {
     ++playerScore.innerHTML;
 }
-function ComputerWins(){
+
+function ComputerWins() {
     ++computerScore.innerHTML;
 }
 
@@ -125,15 +102,37 @@ window.addEventListener('DOMContentLoaded', (event) => {
 
     // Change color every 2 second for word "Game"
 
-    function changeColor(){
+    function changeColor() {
 
         let i = 0;
-        
-        function changed(){
-            document.getElementById("game-heading").style.color = colors[i];
-            i++; 
+
+        function changed() {
+            document.getElementById("game-heading").style.color = COLORS[i];
+            i++;
         }
         setInterval(changed, 2000);
+    }
+    changeColor();
+
+    // Add event listeners to buttons
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            let playerChoice = parseInt(this.getAttribute("data-choice"));
+            playGame(playerChoice);
+        });
+    }
+
+    // Add rules text when clicked
+
+    rulesButton.addEventListener("click", rules);
+
+    function rules() {
+        let rulesText = document.getElementById("rules-open");
+        if (rulesText.classList.contains("hide")) {
+            rulesText.classList.remove("hide");
+        } else {
+            rulesText.classList.add("hide");
         }
-        changeColor();
+    }
 });
